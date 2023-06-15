804f9a9ebc9efdaecbf087fcb27222017bedd4a5
Jon <jonathan.schierl@theoverlab.com>
2023-06-15 17:43:22 -0400

    Restructuring for cleaner code readability

--- zoomed_map.py
+++ zoomed_map.py
@@ -69,10 +69,18 @@ class GarminActivity:
                     if not state in self.STATES:
                         self.STATES.append(state)
 
-    # def get_city(self):
-    #     if hasattr(self, "_city"):
-    #         return self._city
-    
+    def get_city(self):
+        if hasattr(self, "_city"):
+            return self._city
+        else:
+            return None
+            
+    def get_state(self):
+        if hasattr(self, "_state"):
+            return self._state
+        else:
+            return None
+            
     def min_max(self):
         if self.has_location:
             return self.lat_minmax, self.long_minmax
@@ -98,37 +106,22 @@ class FoliumMap:
     def make_map(self, state='None', city='None'):
 
         # If filtered by city or state
+        lat_minmax = [None, None]
+        long_minmax = [None, None]
         if (not state == 'None' or not city == 'None') and len(self.activities) > 0:
-            current_activities = []
-            lat_minmax = [None, None]
-            long_minmax = [None, None]
             for activity in self.activities:
-                applied = False
-                if hasattr(activity, "_city") and hasattr(activity, "_state"):
-                    if (city in activity._city or not city) and (state in activity._state or not state):
-                        current_activities.append(activity)
-                        applied = True
-                elif hasattr(activity, "_city"):
-                    if city in activity._city() or not city:
-                        current_activities.append(activity)
-                        applied = True
-                elif hasattr(activity, "_state"):
-                    if state in activity._state() or not state:
-                        current_activities.append(activity)
-                        applied = True
-                if applied:
-                    if lat_minmax[0] == None or lat_minmax[1] == None or long_minmax[0] == None or long_minmax[1] == None:
-                        lat_minmax, long_minmax = activity.min_max()
-                    else:
-                        lat, long = activity.min_max()
-                        if lat[0] < lat_minmax[0]:                    
-                            lat_minmax[0] = lat[0]
-                        if lat[1] > lat_minmax[1]:
-                            lat_minmax[1] = lat[1]
-                        if long[0] < long_minmax[0]:
-                            long_minmax[0] = long[0] 
-                        if long[1] > long_minmax[1]:
-                            long_minmax[1] = long[1]
+                if lat_minmax[0] == None or lat_minmax[1] == None or long_minmax[0] == None or long_minmax[1] == None:
+                    lat_minmax, long_minmax = activity.min_max()
+                else:
+                    lat, long = activity.min_max()
+                    if lat[0] < lat_minmax[0]:                    
+                        lat_minmax[0] = lat[0]
+                    if lat[1] > lat_minmax[1]:
+                        lat_minmax[1] = lat[1]
+                    if long[0] < long_minmax[0]:
+                        long_minmax[0] = long[0] 
+                    if long[1] > long_minmax[1]:
+                        long_minmax[1] = long[1]
 
         if not (lat_minmax[0] == None or lat_minmax[1] == None or long_minmax[0] == None or long_minmax[1] == None):
             self.center_lat = (sum(lat_minmax)) / 2
@@ -152,7 +145,7 @@ class FoliumMap:
         else:
             raise ValueError("Map hasn't been initialized yet")
 
-    def load_data(self, gpx_path):
+    def load_data(self, gpx_path, state='None', city='None'):
         # Find all .gpx files in the folder
         gpx_files = glob.glob(gpx_path + '/*.gpx')
 
@@ -161,6 +154,7 @@ class FoliumMap:
         # Process each GPX file
         for file_path in gpx_files:
             print(file_path)
+            applied = False
             with open(file_path, 'r') as gpx_file:
                 gpx = gpxpy.parse(gpx_file)
                 points = []
@@ -169,31 +163,53 @@ class FoliumMap:
                         for point in segment.points:
                             lat = point.latitude
                             lon = point.longitude
-                            # ele = point.elevation
+                            ele = point.elevation
                             time = point.time
                             points.append((lat, lon))  # Append coordinates to the points list
-    
-                if len(points) > 2:
-                    self.activity_points.append(points)
 
+            activity = GarminActivity(time, points)
             if len(points) > 2:
-                self.activities.append(GarminActivity(time, points))
+                self.activity_points.append(points)
+                self.activities.append(activity)
+
+                # If we're filtering by city or state, append thes to new lists: "_filtered_activities" and "_filtered_activity_points"
+                if (not state == 'None' or not city == 'None') and len(self.activities) > 0:
+                    filtered = False
+                    if not hasattr(self, "_filtered_activities") and not hasattr(self, "_filtered_activity_points"):
+                        self._filtered_activities = []
+                        self._filtered_activity_points = []
+                    if hasattr(activity, "_city") and hasattr(activity, "_state"):
+                        if (city in activity._city or not city) and (state in activity._state or not state):
+                            filtered = True
+                    elif hasattr(activity, "_city"):
+                        if city in activity._city or not city:
+                            filtered = True
+                    elif hasattr(activity, "_state"):
+                        if state in activity._state or not state:
+                            filtered = True
+                    if filtered:
+                        self._filtered_activity_points.append(points)
+                        self._filtered_activities.append(activity)
 
         print("Loaded.")
 
     def draw_points(self):
-        for activity in self.activity_points:
-            folium.PolyLine(activity).add_to(self.map)
+        if hasattr(self, "_filtered_activity_points"):
+            activities_to_draw = self._filtered_activity_points
+        else:
+            activities_to_draw = self.activity_points
+        for coords in activities_to_draw:
+            folium.PolyLine(coords).add_to(self._map)
 
             # Add markers for the start and finish points
-            start_point = folium.Marker(location=activity[0], popup='Start')
-            start_point.add_to(self.map)
-            end_point = folium.Marker(location=activity[-1], popup='Finish')
-            end_point.add_to(self.map)
+            start_point = folium.Marker(location=coords[0], popup='Start')
+            start_point.add_to(self._map)
+            end_point = folium.Marker(location=coords[-1], popup='Finish')
+            end_point.add_to(self._map)
         
     def save_map(self, mapName="map"):
         if hasattr(self, "_map"):
-            self.save('{}.html'.format(mapName))
+            self._map.save('{}.html'.format(mapName))
         else:
             raise ValueError("Map hasn't been initialized yet")
 
@@ -208,7 +224,7 @@ if __name__ == "__main__":
 
     # Create the map object
     vis_map = FoliumMap()
-    vis_map.load_data(args.gpx_path)
+    vis_map.load_data(args.gpx_path, state=args.state, city=args.city)
 
     print("Cities you ran in: ", GarminActivity.get_cities())
     print("States/Provinces you ran in: ", GarminActivity.get_states())
